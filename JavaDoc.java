2529b965dcd768c6bbf394cbf70db77b94771545
Fakher Zaman <fakherzamanofficial@gmail.com>
2022-10-13 17:19:59 +0500

    first commit
    Author: Fakhar Zaman

---
+++ JavaDoc.java
@@ -0,0 +1,20 @@
+// /**Documentation Comment*/
+
+/**
+ * <i>This is a simple documentation to show that HTML elements can be included in JavaDoc.</i>
+ * @see <a href="https://www.codewithharry.com/videos/java-tutorials-for-beginners-1">Best Java Course Available for <b>Free</b></a>
+ * @author Fakhar Zaman
+ * @version 0.3
+ * @since 2022
+ * @see <a href="https://docs.oracle.com/en/java/javase/14/docs/api/index.html" target="_blank">Java Docs</a>
+ */
+
+public class JavaDoc {
+    public void add(int a, int b) {
+        System.out.println("The sum is : " + a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method!");
+    }
+}
